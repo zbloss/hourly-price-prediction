a4e5015df16e8e9fd8d81e5ebac502cf77dbe176
Zach Bloss <zacharybloss@gmail.com>
2021-06-15 08:53:02 -0400

    init commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Data Science Model for predicting the hourly close price of an asset',
+    author='Zach Bloss',
+    license='',
+)
