225e0d5914a1fda107bd9099f208f0da30d32069
Zach Bloss <zacharybloss@gmail.com>
2021-08-03 08:54:05 -0400

    remove linting

--- hourly_price_prediction/models/utils.py
+++ hourly_price_prediction/models/utils.py
@@ -1,3 +1,4 @@
+import boto3
 import logging
 from math import sqrt
 
@@ -50,13 +51,11 @@ def get_model_class(model_class: str) -> BaseEstimator:
 
     else:
         raise AssertionError("Invalid Model Class passed to --model_class object")
-    logger.info(f"Using {model_object}")
+    logger.info(f'Using {model_object}')
     return model_object
 
 
-def score_metrics(
-    actual_values: np.array, model_predictions: np.array, mode: str
-) -> dict:
+def score_metrics(actual_values: np.array, model_predictions: np.array, mode: str) -> dict:
     """
     Given a set of true values (actual_values) and model predicted values (model_predictions)
     this function will return a dictionary with various regression metrics.
@@ -130,44 +129,40 @@ def train_test_val_split(
         test_targets,
     )
 
-
 def training_pipeline(
-    model,
-    train_features,
-    train_targets,
-    validation_features,
-    validation_targets,
-    test_features,
-    test_targets,
+    model, 
+    train_features, 
+    train_targets, 
+    validation_features, 
+    validation_targets, 
+    test_features, 
+    test_targets
 ) -> list:
     logger = logging.getLogger(__name__)
     model.fit(train_features, train_targets)
-    logger.info("Model Trained")
+    logger.info('Model Trained')
 
     train_predictions = model.predict(train_features)
     validation_predictions = model.predict(validation_features)
     test_predictions = model.predict(test_features)
-    logger.info("Predictions Made")
+    logger.info('Predictions Made')
 
     train_metrics = score_metrics(train_targets, train_predictions, "train")
-    validation_metrics = score_metrics(
-        validation_targets, validation_predictions, "val"
-    )
+    validation_metrics = score_metrics(validation_targets, validation_predictions, "val")
     test_metrics = score_metrics(test_targets, test_predictions, "test")
-    logger.info("Metrics Generated")
+    logger.info('Metrics Generated')
 
     return [train_metrics, validation_metrics, test_metrics]
 
-
 def strategy_simulation(
     model: BaseEstimator,
-    test_dataset: pd.DataFrame,
+    test_dataset: pd.DataFrame, 
     validation_metrics: dict,
     initial_money: int = 100,
     percent_of_total_money_to_move: float = 0.10,
 ) -> tuple:
     """
-    This function tests a simple trading strategy given a model, dataset, and
+    This function tests a simple trading strategy given a model, dataset, and 
     the model's performance dictionary on the validation dataset.
     """
 
@@ -178,38 +173,36 @@ def strategy_simulation(
 
     trading_history = []
     for step, (_, series) in enumerate(test_dataset.iterrows()):
-
+        
         if step == 0:
             total_money = initial_money
-
-        current_close = series["currentclose"]
+        
+        current_close = series['currentclose']
         model_prediction = model.predict(series.values.reshape(1, -1))
 
-        action = "do_nothing"
-        if abs(model_prediction - current_close) > (validation_metrics["mae"] / 3):
+        action = 'do_nothing'
+        if abs(model_prediction - current_close) > (validation_metrics['mae'] / 3):
             if model_prediction - current_close > 0:
-                action = "buy"
+                action = 'buy'
             else:
-                action = "sell"
-
+                action = 'sell'
+        
         amount_of_usd_to_exchange = percent_of_total_money_to_move * total_money
         amount_of_asset_to_exchange = amount_of_usd_to_exchange / current_close
-
+        
         if total_money <= 0:
             break
         else:
-
-            if action == "sell":
+            
+            if action == 'sell':
                 if amount_of_asset_to_exchange > asset_wallet_balance:
                     amount_of_asset_to_exchange = asset_wallet_balance
 
                 total_money += amount_of_asset_to_exchange * current_close
                 asset_wallet_balance -= amount_of_asset_to_exchange
-                assert (
-                    asset_wallet_balance >= 0
-                ), f"asset_wallet_balance is negative: {asset_wallet_balance}"
+                assert asset_wallet_balance >= 0, f'asset_wallet_balance is negative: {asset_wallet_balance}'
 
-            elif action == "buy":
+            elif action == 'buy':
                 if amount_of_usd_to_exchange > total_money:
                     amount_of_usd_to_exchange = total_money
 
@@ -218,25 +211,50 @@ def strategy_simulation(
 
             else:
                 pass
-
+            
             total_assets = asset_wallet_balance * current_close + total_money
-
+        
             actions_this_turn = {
-                "action": action,
-                "amount_of_asset_to_exchange": amount_of_asset_to_exchange,
-                "amount_of_usd_to_exchange": amount_of_usd_to_exchange,
-                "asset_wallet_balance": asset_wallet_balance,
-                "total_money": total_money,
-                "total_assets": total_assets,
+                'action': action,
+                'amount_of_asset_to_exchange': amount_of_asset_to_exchange,
+                'amount_of_usd_to_exchange': amount_of_usd_to_exchange,
+                'asset_wallet_balance': asset_wallet_balance,
+                'total_money': total_money,
+                'total_assets': total_assets
             }
             trading_history.append(actions_this_turn)
-
+            
     trading_history = pd.DataFrame(trading_history)
 
-    final_assets = trading_history["total_assets"].iloc[-1]
+    final_assets = trading_history['total_assets'].iloc[-1]
     difference_between_final_assets_and_initial_money = final_assets - initial_money
-    percentage_gain_lost = (
-        difference_between_final_assets_and_initial_money / initial_money
-    )
+    percentage_gain_lost = difference_between_final_assets_and_initial_money / initial_money
 
     return (trading_history, percentage_gain_lost)
+
+def download_from_s3(bucket: str, key: str, filename: str, region_name: str = 'us-east-2'):
+    """
+    Given a Bucket and Key, this function will download the file
+    and store it at filename.
+    
+    """
+
+    s3_client = boto3.client('s3', region_name=region_name)
+    s3_response = s3_client.download_file(bucket, key, filename)
+    return s3_response
+
+
+def write_to_s3(bucket: str, key: str, filename: str, region_name: str = 'us-east-2'):
+    """
+    Given a Bucket and Key, this function will write the file
+    and to the S3 bucket+key location.
+    
+    """
+
+    s3_client = boto3.client('s3', region_name='us-east-2')
+    s3_response = s3_client.put_object(
+        Bucket=cfg.aws.bucket,
+        Key=f'{base_model_name}/validation_metrics.json',
+        Body=val_metrics_json_file
+    )
+    return s3_response
